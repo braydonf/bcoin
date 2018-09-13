38e58ffa5ce2fb2154f65cb63ac1da2fcad27982
Braydon Fuller <courier@braydon.com>
2018-09-12 18:44:02 -0700

    wallet: monotonic time indexing

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -53,6 +53,10 @@ function nextBlock(wdb) {
   return fakeBlock(wdb.state.height + 1);
 }
 
+function getFakeBlockTime(height) {
+  return 500000000 + (height * (10 * 60));
+}
+
 function fakeBlock(height) {
   const prev = hash256.digest(fromU32((height - 1) >>> 0));
   const hash = hash256.digest(fromU32(height >>> 0));
@@ -62,7 +66,7 @@ function fakeBlock(height) {
     hash: hash,
     prevBlock: prev,
     merkleRoot: root,
-    time: 500000000 + (height * (10 * 60)),
+    time: getFakeBlockTime(height),
     bits: 0,
     nonce: 0,
     height: height
@@ -1222,7 +1226,7 @@ describe('Wallet', function() {
   it('should get range of txs', async () => {
     const wallet = currentWallet;
     const txs = await wallet.getRange(null, {
-      start: util.now() - 1000
+      start: getFakeBlockTime(1)
     });
     assert.strictEqual(txs.length, 2);
   });
@@ -1230,7 +1234,7 @@ describe('Wallet', function() {
   it('should get range of txs from account', async () => {
     const wallet = currentWallet;
     const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
+      start: getFakeBlockTime(1)
     });
     assert.strictEqual(txs.length, 2);
   });
@@ -1363,7 +1367,7 @@ describe('Wallet', function() {
     const wallet = currentWallet;
 
     const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
+      start: getFakeBlockTime(1)
     });
 
     const details = await wallet.toDetails(txs);
@@ -1379,7 +1383,7 @@ describe('Wallet', function() {
     await wallet.rename('test');
 
     const txs = await wallet.getRange('foo', {
-      start: util.now() - 1000
+      start: getFakeBlockTime(1)
     });
 
     const details = await wallet.toDetails(txs);
